397407039d6711f37ec9e07f34d6754af0528049
j.sonar <j.sonar@direction.biz>
2024-12-26 15:38:32 +0530

    function arg added

--- MySQL/mysql_database_backup.sh
+++ MySQL/mysql_database_backup.sh
@@ -6,6 +6,7 @@ DATE_FORMATE=$(date +%Y%m%d_%H%M)
 MYSQLBKP_DIR=/var/backup
 # Require For MGMT Multiple Servers
 CONNECT_DETAILS="_server1"
+CONNECT_DETAILS2="_server2"
 
 function VALIDATE_DATA(){
     # exit script if my.cnf not exist 
@@ -27,10 +28,10 @@ function VALIDATE_DATA(){
 function RUN_DB_BACKUP(){
     cd ${MYSQLBKP_DIR}
     # List All Databases Present and ignore default databases
-    ALL_DB=$(mysql --defaults-group-suffix=${CONNECT_DETAILS} -Bse "show databases" | grep -vE "information_schema|mysql|performance_schema|sys")
+    ALL_DB=$(mysql --defaults-group-suffix=${1} -Bse "show databases" | grep -vE "information_schema|mysql|performance_schema|sys")
     for database in ${ALL_DB}
     do 
-    mysqldump --defaults-group-suffix=${CONNECT_DETAILS} ${database} > ${MYSQLBKP_DIR}/${database}_${DATE_FORMATE}.sql
+    mysqldump --defaults-group-suffix=${1} ${database} > ${MYSQLBKP_DIR}/${database}_${DATE_FORMATE}.sql
     tar zcf ${database}_${DATE_FORMATE}.sql.tar.gz ${database}_${DATE_FORMATE}.sql
     rm -f ${database}_${DATE_FORMATE}.sql
     done
@@ -38,4 +39,5 @@ function RUN_DB_BACKUP(){
 
 ### Function Call ###
 VALIDATE_DATA
-RUN_DB_BACKUP
+RUN_DB_BACKUP ${CONNECT_DETAILS}
+RUN_DB_BACKUP ${CONNECT_DETAILS2}
