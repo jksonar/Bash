8b12c80f06b8f9f6e863acdc19592d4032d5b751
j.sonar <j.sonar@direction.biz>
2024-12-26 14:34:21 +0530

    backup script edit

--- MySQL/mysql_database_backup.sh
+++ MySQL/mysql_database_backup.sh
@@ -3,7 +3,7 @@
 # File Name For Database 20241225_2257
 DATE_FORMATE=$(date +%Y%m%d_%H%M)
 # Backup Directory
-MYSQLBKP_DIR=/var/backup/
+MYSQLBKP_DIR=/var/backup
 # Require For MGMT Multiple Servers
 CONNECT_DETAILS="_server1"
 
@@ -25,13 +25,14 @@ function VALIDATE_DATA(){
 
 
 function RUN_DB_BACKUP(){
+    cd ${MYSQLBKP_DIR}
     # List All Databases Present and ignore default databases
     ALL_DB=$(mysql --defaults-group-suffix=${CONNECT_DETAILS} -Bse "show databases" | grep -vE "information_schema|mysql|performance_schema|sys")
     for database in ${ALL_DB}
     do 
     mysqldump --defaults-group-suffix=${CONNECT_DETAILS} ${database} > ${MYSQLBKP_DIR}/${database}_${DATE_FORMATE}.sql
-    tar zcvf ${MYSQLBKP_DIR}/${database}_${DATE_FORMATE}.sql.tar.gz ${MYSQLBKP_DIR}/${database}_${DATE_FORMATE}.sql
-    rm -f ${MYSQLBKP_DIR}/${database}_${DATE_FORMATE}.sql
+    tar zcf ${database}_${DATE_FORMATE}.sql.tar.gz ${database}_${DATE_FORMATE}.sql
+    rm -f ${database}_${DATE_FORMATE}.sql
     done
 }
 
